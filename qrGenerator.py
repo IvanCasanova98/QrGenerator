282532eb80b633016fb60eec83bfb011c9cc156d
IvanCasanova98 <38133709+IvanCasanova98@users.noreply.github.com>
2019-08-27 03:04:57 -0300

    manejo de errores agreado

--- qrGenerator.py
+++ qrGenerator.py
@@ -11,12 +11,21 @@ def crearqr(nombre):
     )
     qr.add_data(nombre)
     qr.make(fit=True)
-    img = qr.make_image(fill_color="blue", back_color="white")
+    img = qr.make_image(fill_color="black", back_color="white")
     img.save(nombre + '.png')
 
 
+def error():
+    print("Sintax error! \npython qrGenerator.py [nombre] \npython qrGenerator.py [archivo.txt] \n")
+    sys.exit(0)
+
+
+if len(sys.argv) == 1:
+    error()
 if '.txt' in sys.argv[1]:
     file = open(sys.argv[1], "r+")
     listName = file.readlines()
     for name in listName:
         crearqr(name.replace('\n', ""))
+elif sys.argv[1].strip():
+    crearqr(sys.argv[1])
