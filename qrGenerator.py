7b572ccc054901c1018c0d18f8b190ef350a1483
IvanCasanova98 <38133709+IvanCasanova98@users.noreply.github.com>
2019-08-28 01:58:40 -0300

    ahora se puede agregar una imagen para que pegue el codigo qr sobre esa imagen

--- qrGenerator.py
+++ qrGenerator.py
@@ -1,18 +1,33 @@
 import qrcode
 import sys
+from PIL import Image
 
-
-def crearqr(nombre):
+def qrcodepreset():
     qr = qrcode.QRCode(
         version=1,
         error_correction=qrcode.constants.ERROR_CORRECT_L,
         box_size=10,
         border=4,
     )
+    return qr
+
+
+def crearqr(nombre):
+    qr = qrcodepreset()
     qr.add_data(nombre)
     qr.make(fit=True)
-    img = qr.make_image(fill_color="black", back_color="white")
-    img.save(nombre + '.png')
+    imgqr = qr.make_image(fill_color="black", back_color="white")
+    imgqr.save(nombre + '.png')
+
+
+def crearentrarda(nombre, imagen):
+    qr = qrcodepreset()
+    qr.add_data(nombre)
+    qr.make(fit=True)
+    imgqr = qr.make_image(fill_color="black", back_color="white")
+    entrada = Image.open(imagen)
+    entrada.paste(imgqr, (100, 100))
+    entrada.save(nombre + ".jpg")
 
 
 def error():
@@ -20,12 +35,16 @@ def error():
     sys.exit(0)
 
 
-if len(sys.argv) == 1:
+if len(sys.argv) >= 1 & len(sys.argv) <= 2:
+    if '.txt' in sys.argv[1]:
+        file = open(sys.argv[1], "r+")
+        listName = file.readlines()
+        for name in listName:
+            if '.jpg' in sys.argv[2]:
+                crearentrarda(name.replace('\n', ""), sys.argv[2])
+            else:
+                crearqr(name.replace('\n', ""))
+    elif sys.argv[1].strip():
+        crearqr(sys.argv[1])
+else:
     error()
-if '.txt' in sys.argv[1]:
-    file = open(sys.argv[1], "r+")
-    listName = file.readlines()
-    for name in listName:
-        crearqr(name.replace('\n', ""))
-elif sys.argv[1].strip():
-    crearqr(sys.argv[1])
